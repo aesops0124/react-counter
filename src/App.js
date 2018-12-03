205a0bfc7d7eee9c6193dc9e4208511f9c2e5af7
Tommy Hui <aesops0124@gmail.com>
2018-12-03 15:07:20 +0800

    [Tommy] Add add and minor function

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {number:0}
+  addValue = ()=>{this.setState({number:this.state.number+1})}
+  minorValue = ()=>{this.setState({number:this.state.number-1})}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+        <div>
+        <button onClick={this.addValue}>+</button> 
+        {this.state.number}
+        <button onClick={this.minorValue}>-</button> 
+        </div>
     );
   }
 }
